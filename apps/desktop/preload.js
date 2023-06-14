d7a81276f4dcaccc6c63b8b7c7eeda9a6681a5e7
David's Devel <davidsdevel@gmail.com>
2023-06-14 17:58:20 -0430

    build: Remove desktop app from repo

--- apps/desktop/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('utils', {
-  isLogged: () => ipcRenderer.invoke('isLogged'),
-  login: (email, password) => ipcRenderer.invoke('login', {email, password}),
-  onUpdateStorage: cb => ipcRenderer.on('update-storage', cb)
-});
