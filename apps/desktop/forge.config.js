76fb28404e7d769b44331eea6d9e048bd4475ce7
David's Devel <davidsdevel@gmail.com>
2023-06-14 14:18:01 -0430

    build: Update electron forge config

--- apps/desktop/forge.config.js
+++ apps/desktop/forge.config.js
@@ -1,22 +1,32 @@
 module.exports = {
   packagerConfig: {},
   rebuildConfig: {},
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'davidsdevel',
+          name: 'cloud-storage'
+        },
+        prerelease: true
+      }
+    }
+  ],
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
       config: {},
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      "name": "@electron-forge/maker-zip",
+      "platforms": [
+        "linux"
+      ]
     },
     {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
+      "name": "@electron-forge/maker-deb",
+      "config": {}
+    }
   ],
 };
