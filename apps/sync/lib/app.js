e5009ee4df78aa31b1c99b834c603b13aa2e9cfb
David's Devel <davidsdevel@gmail.com>
2023-06-19 11:17:37 -0430

    feat: Added Sentry monitoring

--- apps/sync/lib/app.js
+++ apps/sync/lib/app.js
@@ -1,18 +1,37 @@
 const {createServer} = require('http');
+const Sentry = require('@sentry/node');
 const express = require('express');
 const socket = require('./socket');
 const {join} = require('path');
 const cors = require('cors');
 
+const deviceRouter = require('../routes/device');
+const bucketRouter = require('../routes/bucket');
 const fileRouter = require('../routes/files');
 const authRouter = require('../routes/auth');
-const bucketRouter = require('../routes/bucket');
 
 const app = express();
 
 const http = createServer(app);
 
 const io = socket(http);
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Sentry.Integrations.Express({ app }),
+    // Automatically instrument Node.js libraries and frameworks
+    ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 app.use(express.static(join(__dirname, '../public')));
 app.use(express.json());
@@ -24,10 +43,18 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/auth', authRouter);
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
+app.use('/auth', authRouter);
 app.use('/file', fileRouter);
 app.use('/bucket', bucketRouter);
-app.all('*', (req, res) => res.sendStatus(404))
+app.use('/device', deviceRouter);
+
+app.use(Sentry.Handlers.errorHandler());
+app.use(function onError(err, req, res, next) {
+  res.statusCode = 500;
+  res.end(res.sentry + "\n");
+});
 
 module.exports = http;
